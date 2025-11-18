c32c0c5e884f77caaf0732325c1536e0566ea8db
alexanderoskin <alexander.oskin@gmail.com>
2025-11-18 23:43:20 +0300

    Work on opentelemetry integration

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,16 @@
 import { opentelemetry } from '@elysiajs/opentelemetry';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { PgInstrumentation } from '@opentelemetry/instrumentation-pg';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node';
 
 export const instrumentation = opentelemetry({
+    serviceName: 'sputnik-api',
     instrumentations: [new PgInstrumentation()],
+    spanProcessors: [
+        new BatchSpanProcessor(
+            new OTLPTraceExporter({
+                url: 'http://localhost:4318/v1/traces',
+            }),
+        ),
+    ],
 });
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,3 @@
-import { opentelemetry } from '@elysiajs/opentelemetry';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node';
 import { Elysia } from 'elysia';
 import { instrumentation } from './instrumentation';
 import { addresses } from './modules/addresses';
@@ -9,18 +6,6 @@ import { sentry } from './plugins/sentry';
 
 new Elysia()
     .use(instrumentation)
-    .use(
-        opentelemetry({
-            serviceName: 'sputnik-api',
-            spanProcessors: [
-                new BatchSpanProcessor(
-                    new OTLPTraceExporter({
-                        url: 'http://localhost:4318/v1/traces',
-                    }),
-                ),
-            ],
-        }),
-    )
     .use(sentry())
     .use(language)
     .use(addresses)
