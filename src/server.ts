484aba8d8137082ea4ddfe3dca45e896740003a4
alexanderoskin <alexander.oskin@gmail.com>
2025-11-18 18:37:28 +0300

    Add opentelemetry

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,25 @@
+import { opentelemetry } from '@elysiajs/opentelemetry';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node';
 import { Elysia } from 'elysia';
 import { addresses } from './modules/addresses';
 import { language } from './plugins/language';
 import { sentry } from './plugins/sentry';
 
-new Elysia().use(sentry()).use(language).use(addresses).listen(3001);
+new Elysia()
+    .use(
+        opentelemetry({
+            serviceName: 'sputnik-api',
+            spanProcessors: [
+                new BatchSpanProcessor(
+                    new OTLPTraceExporter({
+                        url: 'http://localhost:4318/v1/traces',
+                    }),
+                ),
+            ],
+        }),
+    )
+    .use(sentry())
+    .use(language)
+    .use(addresses)
+    .listen(3001);
