6aedd16b6a0824aee8ccfffbd4e5b1cac0bb8d70
alexanderoskin <alexander.oskin@gmail.com>
2025-11-19 13:49:50 +0300

    Use custom client for db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,12 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 import { address } from './schema/address';
 
-export const db = drizzle(process.env.DATABASE_URL!, {
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL!,
+});
+
+export const db = drizzle({
+    client: pool,
     schema: { address },
 });
