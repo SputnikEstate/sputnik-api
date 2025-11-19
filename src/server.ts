92618afaa40741494a997c39f90e9d909800c3b6
alexanderoskin <alexander.oskin@gmail.com>
2025-11-19 13:40:17 +0300

    Remove temp flat config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import { Elysia } from 'elysia';
-import type { LanguageContext } from './i18n/config';
 import { instrumentation } from './instrumentation';
-import { querySchema, responseSchema } from './modules/addresses/schemas';
-import { getAddresses } from './modules/addresses/services';
+import { addresses } from './modules/addresses';
 import { language } from './plugins/language';
 import { sentry } from './plugins/sentry';
 
@@ -10,18 +8,5 @@ new Elysia()
     .use(instrumentation)
     .use(sentry())
     .use(language)
-    .get(
-        '/api/addresses',
-        async (ctx) => {
-            const { query, languages } = ctx as typeof ctx & LanguageContext;
-
-            return await getAddresses({ query, languages });
-        },
-        {
-            query: querySchema,
-            response: {
-                200: responseSchema,
-            },
-        },
-    )
+    .use(addresses)
     .listen(3001);
