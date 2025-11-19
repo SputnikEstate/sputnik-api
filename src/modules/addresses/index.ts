9eb478bda4574386d99a0a8684122bc771a31644
alexanderoskin <alexander.oskin@gmail.com>
2025-11-19 15:18:10 +0300

    Code cleanup

--- src/modules/addresses/index.ts
+++ src/modules/addresses/index.ts
@@ -1,19 +1,19 @@
 import { Elysia } from 'elysia';
-import type { LanguageContext } from '../../i18n/config';
+import { language } from '../../plugins/language';
 import { querySchema, responseSchema } from './schemas';
 import { getAddresses as getAddressesService } from './services';
 
-export const addresses = new Elysia({ prefix: '/api/addresses' }).get(
-    '/',
-    async function getAddresses(ctx) {
-        const { query, languages } = ctx as typeof ctx & LanguageContext;
-
-        return await getAddressesService({ query, languages });
-    },
-    {
-        query: querySchema,
-        response: {
-            200: responseSchema,
+export const addresses = new Elysia({ prefix: '/api/addresses' })
+    .use(language)
+    .get(
+        '/',
+        async function getAddresses({ query, languages }) {
+            return await getAddressesService({ query, languages });
         },
-    },
-);
+        {
+            query: querySchema,
+            response: {
+                200: responseSchema,
+            },
+        },
+    );
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,6 @@
 import { Elysia } from 'elysia';
 import { instrumentation } from './instrumentation';
 import { addresses } from './modules/addresses';
-import { language } from './plugins/language';
 import { sentry } from './plugins/sentry';
 
-new Elysia()
-    .use(instrumentation)
-    .use(sentry())
-    .use(language)
-    .use(addresses)
-    .listen(3001);
+new Elysia().use(instrumentation).use(sentry()).use(addresses).listen(3001);
