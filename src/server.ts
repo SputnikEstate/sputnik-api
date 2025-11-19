854049406a7ee0dd3c3a9a806c03d2d34245249c
alexanderoskin <alexander.oskin@gmail.com>
2025-11-19 13:27:28 +0300

    Temp flat config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { Elysia } from 'elysia';
+import type { LanguageContext } from './i18n/config';
 import { instrumentation } from './instrumentation';
-import { addresses } from './modules/addresses';
+import { querySchema, responseSchema } from './modules/addresses/schemas';
+import { getAddresses } from './modules/addresses/services';
 import { language } from './plugins/language';
 import { sentry } from './plugins/sentry';
 
@@ -8,5 +10,18 @@ new Elysia()
     .use(instrumentation)
     .use(sentry())
     .use(language)
-    .use(addresses)
+    .get(
+        '/api/addresses',
+        async (ctx) => {
+            const { query, languages } = ctx as typeof ctx & LanguageContext;
+
+            return await getAddresses({ query, languages });
+        },
+        {
+            query: querySchema,
+            response: {
+                200: responseSchema,
+            },
+        },
+    )
     .listen(3001);
